f37cc6fd1ab9f7482610c180ccef57baf3a890cb
Daniel Zych <danjzych@gmail.com>
2023-09-12 19:23:58 -0500

    Completed tests

--- markov.test.js
+++ markov.test.js
@@ -22,10 +22,48 @@ describe('getChains', function () {
         "the": ["hat."],
         "hat.": [null],
       }
-    );
+      );
+    });
+
   });
 
+
+  //test for getText
+  describe('getText', function () {
+    let firstWord;
+    let chain;
+    let text;
+
+    beforeAll(function () {
+      source = 'The cat is in the hat. The cat is the cat. The hat is a cat.';
+      machine = new MarkovMachine(source);
+
+      firstWord = machine.words[0];
+      chain = machine.getChains()
+    });
+
+    beforeEach(function() {
+      text = machine.getText();
+    })
+
+    test('text starts with correct word', function() {
+      const testFirstWord = text.split(' ')[0];
+
+      expect(testFirstWord).toEqual(firstWord);
+    })
+
+    test('has valid second word', function () {
+      const testFirstWord = text.split(' ')[0];
+      const testSecondWord = text.split(' ')[1];
+
+      const containsWord = chain[testFirstWord].includes(testSecondWord);
+      expect(containsWord).toEqual(true);
+    });
+
+    test('text ends with correct word', function () {
+      const testLastWord = text.split(' ').splice(-1);
+
+      const containsNull = chain[testLastWord].includes(null);
+      expect(containsNull).toEqual(true);
+    });
 });
-
-
-//test for getText
